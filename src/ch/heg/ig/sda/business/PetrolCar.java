5bbd943db7163fd6554cf0f2e2ba6a301ce0e1e3
Thibault Libert <thibault.libert@he-arc.ch>
2019-12-16 11:05:24 +0100

    Generate code from VP (2)

--- src/ch/heg/ig/sda/business/PetrolCar.java
+++ src/ch/heg/ig/sda/business/PetrolCar.java
@@ -1,4 +1,14 @@
 package ch.heg.ig.sda.business;
 
+import ch.heg.ig.sda.business.Vehicle;
+
 public class PetrolCar extends Vehicle {
+
+    private float tankCapacity;
+
+    public PetrolCar() {
+        // TODO - implement PetrolCar.PetrolCar
+        throw new UnsupportedOperationException();
+    }
+
 }
--- src/ch/heg/ig/sda/business/Vehicle.java
+++ src/ch/heg/ig/sda/business/Vehicle.java
@@ -1,5 +1,19 @@
 package ch.heg.ig.sda.business;
 
+import ch.heg.ig.sda.business.Parking;
+
 public abstract class Vehicle {
+
+    private Parking parking;
+    private int id;
+    private String registrationPlate;
+    private String model;
+    private int personCapacity;
+    private String brand;
+
+    public Vehicle() {
+        // TODO - implement Vehicle.Vehicle
+        throw new UnsupportedOperationException();
+    }
 
 }
