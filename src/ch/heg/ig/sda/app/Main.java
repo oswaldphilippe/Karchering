6a663856165363258681bc7aa07d7dd8226cf3ab
thibault.libert <thibault.libert@he-arc.ch>
2020-01-14 11:01:48 +0100

    Evaluate loading time of parkings

--- src/ch/heg/ig/sda/app/Main.java
+++ src/ch/heg/ig/sda/app/Main.java
@@ -13,41 +13,33 @@ public class Main {
     }
 
     private void run() {
-        loadParkings();
-        createVehicles();
+        evaluateParkingLoadingTime();
     }
 
     public static void main(String[] args) { new Main().run(); }
 
-    private void createVehicles() {
-        Vehicle car1 = new ElectricCar(1, "NE12341", "Tesla 2", 5, "Tesla", 900);
-        Vehicle car2 = new ElectricCar(2, "NE12342", "Tesla 3", 5, "Tesla", 1000);
-        Vehicle car3 = new ElectricCar(3, "NE12343", "Tesla 2", 5, "Tesla", 900);
-        Vehicle car4 = new ElectricCar(4, "NE12344", "Tesla 2", 5, "Tesla", 900);
 
-        Vehicle car5 = new PetrolCar(5, "NE12345", "Golf 2", 5, "Volkswagen", 60);
-        Vehicle car6 = new PetrolCar(6, "NE12346", "Polo", 5, "Volkswagen", 60);
-        Vehicle car7 = new PetrolCar(7, "NE12347", "TT", 5, "Audi", 60);
-        Vehicle car8 = new PetrolCar(8, "NE12348", "Golf 2", 5, "Volkswagen", 60);
+    private void evaluateParkingLoadingTime() {
+        System.out.println("Chargement des parkings...");
+        long startTime = System.currentTimeMillis();
+        int nbParkings = loadParkings();
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
 
-        park.addVehicle(car1);
-        park.addVehicle(car2);
-        park.addVehicle(car3);
-        park.addVehicle(car4);
-        park.addVehicle(car5);
-        park.addVehicle(car6);
-        park.addVehicle(car7);
-        park.addVehicle(car8);
+        System.out.println("Temps de chargement des " + nbParkings + " parkings : " + elapsedTime + "ms");
     }
 
-    private void loadParkings() {
+    private void evaluateVehiclesLoadingTime() {
+
+    }
+
+    private int loadParkings() {
         String csvFile = "./resources/400parkings.csv";
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
 
         try {
-
             br = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile), "UTF-8"));
 
             int i = 0;
@@ -69,6 +61,7 @@ public class Main {
                 i++;
             }
             System.out.println("Added " + (i - 1) + " parkings to park");
+            return i-1;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
 
@@ -84,5 +77,10 @@ public class Main {
                 }
             }
         }
+        return 0;
+    }
+
+    private void loadVehicles() {
+
     }
 }
