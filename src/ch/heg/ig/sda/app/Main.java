d090c78b2d8d9f3123a79895d975c630009a3f5e
RuegAdrien <adrien.ruegger@gmail.com>
2020-01-16 14:25:42 +0100

    added time evaluations adding and getting a single parking, and adding a vehicle to a parking

--- src/ch/heg/ig/sda/app/Main.java
+++ src/ch/heg/ig/sda/app/Main.java
@@ -6,6 +6,8 @@ import ch.heg.ig.sda.data.ElectricCarDataLoader;
 import ch.heg.ig.sda.data.ParkingDataLoader;
 import ch.heg.ig.sda.data.PetrolCarDataLoader;
 
+import java.util.List;
+
 public class Main {
 
     private Park park;
@@ -17,6 +19,9 @@ public class Main {
     private void run() {
         evaluateParkingLoadingTime();
         evaluateVehiclesLoadingTime();
+        evaluateGetParkingsTime();
+        evaluateAddParkingTime();
+        evaluateAddVehicleToParkingTime();
     }
 
     public static void main(String[] args) { new Main().run(); }
@@ -40,6 +45,40 @@ public class Main {
         long elapsedTime = endTime - startTime;
 
         System.out.println("Temps de chargement des " + nbVehicles + " véhicules : " + elapsedTime + "ms");
+    }
+
+    private void evaluateGetParkingsTime() {
+        System.out.println("Obtention des parkings...");
+        long startTime = System.currentTimeMillis();
+        park.getParkings();
+        long endTime = System.currentTimeMillis();
+        int nbParkings = park.getParkings().size();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Temps d'obtention des " + nbParkings + " Parkings : " + elapsedTime + "ms");
+    }
+
+    private void evaluateAddParkingTime() {
+        System.out.println("Ajout d'un parking...");
+        Parking p = new Parking(12,"rue de la colline 3","2202",234.5,295.4,20);
+        long startTime = System.currentTimeMillis();
+        park.addParking(p);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Temps d'ajout d'un parking : " + elapsedTime + "ms");
+    }
+
+    private void evaluateAddVehicleToParkingTime() {
+        System.out.println("Ajout d'un véhicule au parking...");
+        Parking p = park.getParkings().get(12);
+        Vehicle v = new PetrolCar(12,"NE1369","Duster",5,"Dacia",80);
+        long startTime = System.currentTimeMillis();
+        park.addVehicleToParking(v,p);
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Temps d'ajout d'un véhicule dans un parking : " + elapsedTime + "ms");
     }
 
     private int loadParkings() {
